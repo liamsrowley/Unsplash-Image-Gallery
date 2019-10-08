74962f1b9dc4ce5c9ad04bf45b448cb5906e82c4
Liam Rowley <liam.rowley@hotmail.co.uk>
2019-10-08 11:28:30 +0100

    Added infinite loading for images

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 
 import unsplash from '../apis/unsplash';
 
@@ -14,36 +14,69 @@ const App = () => {
   const [response, setResponse] = useState({
     total: null,
   });
+  const [query, setQuery] = useState('');
+  const [images, setImages] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
 
-  const handleSubmit = async (query, page = 1) => {
+  const fetchImages = async (query, page = 1, per_page = 20) => {
     setIsLoading(true);
-    const response = await unsplash.get(`/search/photos/`, {
+
+    const config = {
       params: {
         query,
         page,
-        per_page: 20
+        per_page
       }
-    });
-    setResponse(response.data);
+    }
+
+    const response = await unsplash.get('/search/photos/', config);
     setIsLoading(false);
+    return response.data;
+  }
+
+  const loadMoreImages = async () => {
+    const nextPage = response.current_page + 1;
+    const data = await fetchImages(query, nextPage);
+    setImages([ ...images, ...data.results ]);
+    setResponse({
+      total: data.total,
+      total_pages: data.total_pages,
+      current_page: nextPage
+    });
+    console.log(images);
+  }
+
+  const handleSubmit = async (query) => {
+    setQuery(query);
+    const data = await fetchImages(query);
+    setResponse({
+      total: data.total,
+      total_pages: data.total_pages,
+      current_page: 1
+    });
+    setImages(data.results);
   }
 
   const renderPageTitle = () => {
     if (response.total && response.total !== 0 && !isLoading) {
-      return <h2 className="page__title">Found {response.total} results for this search term</h2>
+      return <h2 className="page__title">Found {response.total.toLocaleString()} results for this search term</h2>
     }
   }
 
   const renderPageContent = () => {
-    if (isLoading) {
-      return <Loader className="loader--big"/>;
-    }
-    if (response.results) {
-      if (response.results.length === 0) {
+    if (images) {
+      if (images.length === 0) {
         return <PagePlaceholder imageURL="/images/nodata.svg" message="Oops! We couldn't find any results for that search term" imageAlt="Clipboard with blank document" />
       }
-      return <ImageList images={response.results} isLoading={isLoading} />
+      return (
+        <Fragment>
+          <ImageList images={images} isLoading={isLoading} />
+          <button className="button" onClick={loadMoreImages}>
+            { isLoading && <Loader /> }
+            { !isLoading && 'Load More' }
+          </button>
+        </Fragment>
+      )
     }
     return <PagePlaceholder imageURL="/images/search.svg" message="Get started by searching for something using the search bar above." imageAlt="Magnifying glass on paper" />
   }
@@ -53,6 +86,7 @@ const App = () => {
       <Search onSubmit={handleSubmit} isLoading={isLoading}/>
       { renderPageTitle() }
       { renderPageContent() }
+
     </div>
   );
 }
